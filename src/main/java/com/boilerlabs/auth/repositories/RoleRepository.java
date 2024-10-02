f58a2ede49578e30cf2616c71c9cec67563e4726
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-10-02 12:01:19 -0300

    feat: add AuthService

--- src/main/java/com/boilerlabs/auth/repositories/RoleRepository.java
+++ src/main/java/com/boilerlabs/auth/repositories/RoleRepository.java
@@ -1,9 +1,13 @@
 package com.boilerlabs.auth.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.boilerlabs.auth.entities.Role;
 
 public interface RoleRepository extends MongoRepository<Role, String> {
+    Optional<Role> findByName(String name);
 
+    boolean existsByName(String name);
 }
--- src/main/java/com/boilerlabs/auth/repositories/UserRepository.java
+++ src/main/java/com/boilerlabs/auth/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.boilerlabs.auth.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.boilerlabs.auth.entities.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
 
+    boolean existsByUsername(String username);
 }
