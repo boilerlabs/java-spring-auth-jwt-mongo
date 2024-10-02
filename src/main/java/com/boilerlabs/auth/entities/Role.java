6fffb12b8a85f8f7f0837d5bc18cebb3851b4a35
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-10-02 11:52:19 -0300

    feat: add Role

---
+++ src/main/java/com/boilerlabs/auth/entities/Role.java
@@ -0,0 +1,44 @@
+package com.boilerlabs.auth.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class Role {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Role(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
