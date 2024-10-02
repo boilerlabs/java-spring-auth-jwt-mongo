b8c15a9c2431f57e9f8c24627ebfbcfccc257997
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-10-02 12:22:31 -0300

    feat: add DataInitializer

--- src/main/java/com/boilerlabs/auth/entities/Role.java
+++ src/main/java/com/boilerlabs/auth/entities/Role.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "users")
+@Document(collection = "roles")
 public class Role {
 
     @Id
